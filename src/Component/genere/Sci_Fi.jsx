ebb757e3753181f3f91ec944417aedfc712759f3
Abenezer <150917473+Abu388@users.noreply.github.com>
2024-03-17 15:41:52 +0300

    fully function genres

--- src/Component/genere/Sci_Fi.jsx
+++ src/Component/genere/Sci_Fi.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
-
+import { useState } from 'react';
+import { useEffect } from 'react';
 const Sci_Fi = () => {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    getMovies();
+   
+}, []);
+
+
+const getMovies = () => {
+  
+  fetch("https://api.themoviedb.org/3/discover/movie?api_key=cd03c49615cd326a66da1a5a7b3cc69e&language=en-US&sort_by=release_date.desc&page=1&with_genres=878")
+    .then(res => res.json())
+    .then(json => {
+      setMovieList(json.results);
+     
+    })
+    .catch(error => {
+      console.error('Error fetching movies:', error);
+      
+    });
+}
   return (
-    <div>Sci_Fi</div>
+    <div style={{ marginTop: '500px' }}>
+    
+    {
+      movieList.map((movie,index) => (
+        movie.poster_path && (
+          <div key={index} style={{ display: "inline-block" }}>
+            <div style={{margin:'30px'}}> <img style={{ width: "300px", height: "250px" }} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={`Movie ${index}`} />
+           </div>
+           
+             </div>
+        )
+      )
+    )}
+  </div>
   )
 }
 
